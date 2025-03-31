75848755a0e43b0f8bef87078d2f3bff5e2d88ae
Yari Herrera <yari.andres@gmail.com>
2025-03-31 10:32:55 +0200

    updates index page

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <UContainer class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold">Nuxt Chat</h1>
+    <UButton to="/chat" label="Start a chat" />
+  </UContainer>
+</template>
