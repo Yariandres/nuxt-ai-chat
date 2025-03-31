d2230b185018a9416e3d98cc0af26fbe1b78c536
Yari Herrera <yari.andres@gmail.com>
2025-03-31 14:44:20 +0200

    updates client component to pass props

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const { sendMessage, messages, chat } = useChat();
+
+useHead({
+  title: chat.value.title,
+});
+</script>
+
 <template>
-  <ChatWindow />
+  <ChatWindow :messages="messages" :chat @send-message="sendMessage" />
 </template>
