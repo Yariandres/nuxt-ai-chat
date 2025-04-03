a25481bfa6e90e929eab29a2ea5e172ee3cc74ae
Yari Herrera <yari.andres@gmail.com>
2025-04-03 16:35:47 +0200

    updates end point to use ai service

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,13 +1,26 @@
+import {
+  createOpenAIModel,
+  generateChatResponse,
+  createOllamaModel,
+} from '../services/ai-service';
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { messages } = body;
 
   const id = messages.length.toString();
-  const lastMessage = messages[messages.length - 1];
+  // const lastMessage = messages[messages.length - 1];
+
+  const openaiApiKey = useRuntimeConfig().NUXT_OPENAI_API_KEY;
+  const openaiModel = createOpenAIModel(openaiApiKey);
+
+  const ollamaModel = createOllamaModel();
+
+  const response = await generateChatResponse(openaiModel, messages);
 
   return {
     id,
     role: 'assistant',
-    content: `(server) You said: ${lastMessage.content}`,
+    content: response,
   };
 });
