751efa0940b0b0152e1e471feab18e7eb3c055e9
Yari Herrera <yari.andres@gmail.com>
2025-05-01 12:20:59 +0200

    updated configs files

--- layers/chat/nuxt.config.ts
+++ layers/chat/nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
-  modules: ['@nuxt/eslint'],
+
+  runtimeConfig: {
+    NUXT_OPENAI_API_KEY: process.env.NUXT_OPENAI_API_KEY,
+  },
 });
